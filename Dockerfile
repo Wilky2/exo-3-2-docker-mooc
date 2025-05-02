78574d7d3ed939dd115a55d81b22573d0f18b5cc
Wilky2 <wilkyclervil@yahoo.com>
2025-05-01 21:05:04 -0400

    adding files

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+#docker file
+FROM amazoncorretto:17
+
+EXPOSE 8080
+
+WORKDIR /usr/src/app
+
+RUN yum install -y tar && yum install -y gzip
+
+COPY . .
+
+RUN ./mvnw package
+
+ENTRYPOINT ["java"]
+
+CMD ["-jar", "./target/docker-example-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/wilky/docker/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.wilky.docker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@GetMapping("/")
+	public String hello(Model model) {
+		return "hello";
+	}
+
+}
